dd2e9b117e8bd6d89ea29c90c4aa0a4d137a9e81
Gabriel <mathematicalmethods@proton.me>
2025-01-21 21:45:16 -0500

    Added streamer information processing loop

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
 import schwabdev 
 import os
 import logging
+import time
+import json
 from dotenv import load_dotenv
 
-# Here are initial set-up steps to initialize Trader.
-def initialize():
+
+shared_list = []
+def response_handler(message):
+    shared_list.append(message)
+
+# Trader 
+def main():
+
     # load env vars from .env file
     load_dotenv()
     
     # set logging level
     logging.basicConfig(level=logging.INFO) 
     
-    # create client
+    # create client and streamer
     client = schwabdev.Client(app_key=os.getenv('app_key'),
                               app_secret=os.getenv('app_secret'),
                               callback_url=os.getenv('callback_url'))
-
-    # create streamer
     streamer = client.stream
 
-def example_response_handler(message):
-    print("Test" + message)
+    # start streamer and send request for what symbols we want.
+    streamer.start(response_handler)
+    streamer.send(streamer.level_one_equities("AMD", "0", "ADD"))
 
+    secondoldest_response = 0
 
+    # create a while loop to handle reading the streamer
+    while True:
+        while len(shared_list) > 0:
+            oldest_response = json.loads(shared_list.pop(0)) # load oldest data from the list.
+            for rtype, services in oldest_response.items():
+                if rtype == "data":
+                    print(oldest_response)
+                    for service in services:
+                        print(service)
+                elif rtype == "response":
+                    print(oldest_response)
+                elif rtype == "notify":
+                    print(oldest_response['notify'][0]['heartbeat'])
 
-# Trader 
-def main():
-
-    # Initialize the variables / objects of trader.
-    initialize()
-    
-    # We need something to begin the streamer, so we do:
-    streamer.start(example_response_handler)
-
-
-
+        time.sleep(0.5) # slow down check of list length.
 
 
 if __name__ == '__main__':
