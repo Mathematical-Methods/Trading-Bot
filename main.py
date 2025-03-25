f80652d3487871ed202adfcfae69dce5242a49b1
Gabriel <mathematicalmethods@proton.me>
2025-03-24 20:31:56 -0500

    Added client to TradingBot class.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import schwabdev
+import os
+from dotenv import load_dotenv
+
 
 class TradingBot:
-    def __init__(self, cash, symbol):
-        self.cash = cash
-        self.holdings = 0
+    def __init__(self, app_key, app_secret, symbol, simulate):
+        self.app_key = app_key
+        self.app_secret = app_secret
         self.symbol = symbol
+        self.client = schwabdev.Client(app_key, app_secret,'https://127.0.0.1')
     
     def decide_action(self, price):
         if price < self.buy_threshold:
@@ -23,4 +27,8 @@ class TradingBot:
             self.cash += price
             self.holdings -= 1
     
-        
+
+if __name__ == '__main__':
+    load_dotenv()
+    bot = TradingBot(os.getenv('app_key'), os.getenv('app_secret'), "APPL", simulate=True)
+    bot.run()
